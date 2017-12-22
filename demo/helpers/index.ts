f048bbf18166fd5632659adc28c44aa512f3727b
lzxb <1340641314@qq.com>
2017-12-22 21:44:19 +0800

    feat(rotating-square): 旋转的正方形

--- demo/helpers/index.ts
+++ demo/helpers/index.ts
@@ -1,3 +1,7 @@
+export const clearCanavs: Function = (ctx: CanvasRenderingContext2D) => {
+  ctx.canvas.width = ctx.canvas.width
+}
+
 export const createApp: Function = (): CanvasRenderingContext2D => {
   const app: HTMLCanvasElement = document.createElement('canvas')
   const body: HTMLElement = document.body
@@ -8,4 +12,23 @@ export const createApp: Function = (): CanvasRenderingContext2D => {
   body.style.margin = '0'
   body.style.overflow = 'hidden'
   return app.getContext('2d')!
+}
+
+export const drawPosition = (ctx: CanvasRenderingContext2D, x: number, y: number, color: string = 'red') => {
+  ctx.beginPath()
+  ctx.moveTo(x - 10, y)
+  ctx.lineTo(x + 10, y)
+  ctx.moveTo(x, y - 10)
+  ctx.lineTo(x, y + 10)
+  ctx.closePath()
+  ctx.strokeStyle = color
+  ctx.stroke()
+}
+
+export const animation = (draw: Function) => {
+  const wrapDraw: FrameRequestCallback = () => {
+    draw()
+    window.requestAnimationFrame(wrapDraw)
+  }
+  window.requestAnimationFrame(wrapDraw)
 }
